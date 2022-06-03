03f684d65121f3cc23e3a52acf07790aefba342d
unknown <jasoniyi>
2022-06-03 11:09:24 +0100

    added logic for form state

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Users should be able to:
 
 ### Screenshot
 
-![](./Screenshot.jpg)
+![](./Screenshot.png)
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
@@ -29,8 +29,8 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Add solution URL here](https://github.com/Jasoniyi/url-shortening-api-project)
+- Live Site URL: [Add live site URL here](https://url-shortening-api-project.vercel.app/)
 
 ## My process
 
--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,7 @@ const About = () => {
     const [getValue, setGetValue ] = useState('')
     const [urlsHistory, setUrlsHistory] = useState([])
     const [datas, setData ] = useState('')
+    const[inputError, setInputError] = useState(false)
 
     const getInput = (e) => {
         const values = e.target.value
@@ -20,15 +21,20 @@ const About = () => {
 
     // get data from api
         const getShortCode = async () => {
-          try {
-            const data = await axios.get(`https://api.shrtco.de/v2/shorten?url=${getValue}`)
-            const result = data.data.result
-            console.log('were', result)
-            setData(result)
-            console.log('data', datas)
-          } catch (err) {
-            return err.message
-          }
+            let validateValue = /[(http(s)?):(www)?a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/
+            if(getValue.match(validateValue)) {
+              try {
+                const data = await axios.get(`https://api.shrtco.de/v2/shorten?url=${getValue}`)
+                const result = data.data.result
+                console.log('were', result)
+                setData(result)
+                console.log('data', datas)
+                } catch (err) {
+                    return err.message
+                }  
+                    } else {
+                        console.log('url not valid')
+                    }
         }
        
     
@@ -44,8 +50,15 @@ const About = () => {
 
     const handleSubmit = (e) => {
             e.preventDefault()
-            getShortCode()
-            setGetValue('')
+            if(getValue.length < 1) {
+                setInputError(true)
+            } else {
+                getShortCode()
+                setGetValue('')
+                console.log('check', getValue.length)  
+            }
+            
+            
         }
 
     useEffect(() => {
@@ -63,31 +76,44 @@ const About = () => {
 
   return (
     <div className='bg-shortlyGray py-12'>
-        <div className='w-9/12 md:h-24 h-36 bg-shortlyDarkViolet mx-auto rounded-md bg-hero-pattern flex justify-center md:first-letter md:-mt-20'>
+        <div className='w-9/12 md:h-28 h-36 bg-shortlyDarkViolet mx-auto rounded-md bg-hero-pattern flex justify-center md:first-letter md:-mt-20'>
             <form className='m-auto flex flex-col md:flex-row items-center' onSubmit={handleSubmit}>
-                <input 
-                type="text"
-                placeholder='Shorten a link here...'
-                className='md:w-128 w-64 h-11 rounded-md px-2 md:my-0 my-3'
-                value={getValue}
-                onChange={getInput}
-                />
+                {
+                     inputError ? 
+                     <div className='flex flex-col'>
+                     <input 
+                        type="text"
+                        placeholder='Shorten a link here...'
+                        className='md:w-128 w-64 h-11 rounded-md px-2 md:my-0 md:mt-8 '
+                        value={getValue}
+                        onChange={getInput}
+                        
+                        />
+                     <p className='italic text-red-500 py-1'> please add a link</p>
+                     </div>
+                     :
+                     <input 
+                        type="text"
+                        placeholder='Shorten a link here...'
+                        className='md:w-128 w-64 h-11 rounded-md px-2 md:my-0 my-3'
+                        value={getValue}
+                        onChange={getInput}
+                     />
+                }
                 <button className='bg-shortlyCyan md:ml-6 md:px-4 text-white rounded-md h-11 md:w-44 w-64'>Shorten it!</button>
             </form>
         </div>
-
-        {
-            urlsHistory.length > 0 && 
-                urlsHistory.map((url, i) => (
-                    <div className='w-9/12 my-4 bg-white mx-auto rounded-md'>
-                       <CardList key={i} 
-                         originalLink={url.original_link}
-                         shortenedLink = {url.full_short_link} /> 
-                    </div>
-                ))
-           
-        }
-                
+            {
+                urlsHistory.length > 0 && 
+                    urlsHistory.map((url, i) => (
+                        <div className='w-9/12 my-4 bg-white mx-auto rounded-md'>
+                        <CardList key={i} 
+                            originalLink={url.original_link}
+                            shortenedLink = {url.full_short_link} /> 
+                        </div>
+                    ))
+            
+            }
         <div className='flex flex-col items-center '>
             <div className='flex flex-col justify-center pt-8 items-center my-8'>
                 <h2 className='md:text-4xl text-3xl font-bold text-shortlyVeryDarkViolet py-4'>Advanced Statistics</h2>
